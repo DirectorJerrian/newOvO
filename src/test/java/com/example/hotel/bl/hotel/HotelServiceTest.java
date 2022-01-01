c8d6fcd953c74e2f90cdc3305c0ecef67cc81809
zhangyue <181250188@nju.edu.cn>
2022-01-01 20:10:11 +0800

    fix: test修正

--- src/test/java/com/example/hotel/bl/hotel/HotelServiceTest.java
+++ src/test/java/com/example/hotel/bl/hotel/HotelServiceTest.java
@@ -4,7 +4,6 @@ package com.example.hotel.bl.hotel;
 import com.example.hotel.data.hotel.HotelMapper;
 import com.example.hotel.po.Hotel;
 import com.example.hotel.util.ServiceException;
-import com.example.hotel.vo.HotelSearchVO;
 import com.example.hotel.vo.HotelVO;
 import org.junit.Assert;
 import org.junit.Test;
@@ -25,25 +24,12 @@ public class HotelServiceTest {
     @Autowired
     HotelMapper hotelMapper;
 
-    @Test
-    public void hotelSearchTest(){
-        HotelSearchVO hotelSearchVO=new HotelSearchVO();
-        hotelSearchVO.setHotelName("神仙酒店");
-        hotelSearchVO.setStar(0);
-        hotelSearchVO.setRate(0);
-        hotelSearchVO.setRoomType("0");
-        hotelSearchVO.setBizRegion("1343296078");
-        List<HotelVO> hotellist=hotelService.hotelSearch(hotelSearchVO);
-        assertEquals(1,hotellist.size());
-        assertEquals("神仙酒店",hotellist.get(0).getName());
-    }
 
     @Test
     public void updateHotelInfo(){
         hotelService.updateHotelInfo(1,"店酒庭汉","Nadix","住入您迎欢");
         HotelVO target=hotelMapper.selectById(1);
         assertEquals("店酒庭汉",target.getName());
-        assertEquals("Nadix",target.getBizRegion());
         assertEquals("住入您迎欢",target.getDescription());
     }
 
@@ -51,7 +37,6 @@ public class HotelServiceTest {
     public void addHotelTest() throws ServiceException {
         HotelVO hotelVO=new HotelVO();
         hotelVO.setAddress("hululu");
-        hotelVO.setBizRegion("XiDan");
         hotelVO.setDescription("欢迎您的光临，这里的风景很不错");
         hotelVO.setHotelStar("Five");
         hotelVO.setManagerId(1);
@@ -64,7 +49,6 @@ public class HotelServiceTest {
                 target=item;
             }
         }
-        Assert.assertEquals(target.getBizRegion(),"XiDan");
         Assert.assertEquals((int)target.getManagerId(),1);
         Assert.assertEquals(target.getDescription(),"欢迎您的光临，这里的风景很不错");
         Assert.assertEquals(target.getHotelStar(),"Five");
